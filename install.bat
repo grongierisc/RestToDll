7aa3f896d3550574bfde6f82ce7c40f9eaa614f0
Melissa Li <melissa.li@epita.fr>
2019-05-03 11:42:59 +0200

    [PRDUCTION] Added path to dll in Install Manifest

--- install.bat
+++ install.bat
@@ -18,14 +18,14 @@ set NAMESPACE_TO_CREATE=DEMO
 set SOURCE_DIR=RestToDll\src
 
 :: Build and import application to IRIS
+copy Allergie\Allergie.dll %IRIS_DIR%\mgr\
+
 echo Importing project...
 (
-echo %USERNAME%
-echo %PASSWORD%
 echo zn "%NAMESPACE%" set st = $system.Status.GetErrorText($system.OBJ.ImportDir("%~dp0%BUILD_DIR%",,"ck",,1^^^)^^^) w "IMPORT STATUS: "_$case(st="",1:"OK",:st^^^), ! 
 echo set pVars("NAMESPACE"^^^) = "%NAMESPACE_TO_CREATE%"
 echo set pVars("SourceDir"^^^) = "%~dp0%SOURCE_DIR%"
-echo zw ##class(App.Installer^^^).setup(.pVars^^^)
+echo do ##class(App.Installer^^^).setup(.pVars^^^)
 echo halt
 ) | "%IRIS_DIR%\bin\irisdb.exe" -s "%IRIS_DIR%\mgr" -U %NAMESPACE%
 
